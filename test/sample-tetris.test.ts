b5f3a4c69006689adfd8820856d16e49e5e0025f
Kanta Kurzumi <krzmknt@gmail.com>
2024-08-21 02:34:00 +0900

    Initial commit

---
+++ test/sample-tetris.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SampleTetris from '../lib/sample-tetris-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sample-tetris-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SampleTetris.SampleTetrisStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
